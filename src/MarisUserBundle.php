d40904c7168dcc58efbc5a3965c88c03360b810e
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-23 11:36:55 +0300

    3

--- src/MarisUserBundle.php
+++ src/MarisUserBundle.php
@@ -2,9 +2,14 @@
 
 namespace Maris\Symfony\User;
 
+use Maris\Symfony\User\DependencyInjection\MarisUserExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class MarisUserBundle extends AbstractBundle
 {
-
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new MarisUserExtension();
+    }
 }
