58fe9f613d4088805a7bb99cf5e14f25958a8b06
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-23 11:43:39 +0300

    4

--- src/MarisUserBundle.php
+++ src/MarisUserBundle.php
@@ -8,6 +8,11 @@ use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class MarisUserBundle extends AbstractBundle
 {
+    public function getPath(): string
+    {
+        return dirname(__DIR__ );
+    }
+
     public function getContainerExtension(): ?ExtensionInterface
     {
         return new MarisUserExtension();
