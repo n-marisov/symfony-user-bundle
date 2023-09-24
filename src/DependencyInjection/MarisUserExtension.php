fca48c577707880472e80e910beeb3016b4dfad4
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-24 10:45:24 +0300

    25

--- src/DependencyInjection/MarisUserExtension.php
+++ src/DependencyInjection/MarisUserExtension.php
@@ -18,6 +18,6 @@ class MarisUserExtension extends Extension
         $path = realpath( dirname(__DIR__).'/../config' );
         $loader = new YamlFileLoader( $container, new FileLocator( $path ) );
         $loader->load('services.yaml');
-        $loader->load('twig.yaml');
+       // $loader->load('twig.yaml');
     }
 }
