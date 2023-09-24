3a0b0123dd5d97c4529506d5bd5dd76526a9a0ca
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-24 17:35:24 +0300

    44

--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -5,6 +5,7 @@ namespace Maris\Symfony\User\Form;
 use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 /**
@@ -15,6 +16,7 @@ class LoginFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options):void
     {
         $builder->add("phone",PhoneNumberType::class,["default_region"=>"RU"])
-            ->add("password",PasswordType::class);
+            ->add("password",PasswordType::class)
+            ->add("submit",SubmitType::class);
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,8 +2,10 @@
 
 namespace Maris\Symfony\User\Form;
 
+use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationFormType extends AbstractType
@@ -11,9 +13,10 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options):void
     {
         $builder
-            ->add('phone')
+            ->add('phone',PhoneNumberType::class,['default_region'=>"RU"])
             ->add("person",PersonFormType::class)
-            ->add("password",PasswordType::class);
+            ->add("password",PasswordType::class)
+            ->add("submit",SubmitType::class);
         $builder->get("person")->addViewTransformer( new PersonViewTransformer() );
     }
 }
