a6879984df8d6dd74e582ac212d023e514d8d2bd
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-24 09:38:10 +0300

    21

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -32,7 +32,7 @@ class LoginController extends AbstractController
         }*/
 
         // Форма не отправлена.
-        return $this->render("/login/index.html.twig",[
+        return $this->render(__DIR__. "/../../templates" ."/login/index.html.twig",[
 
         ]);
         /*return $this->render("@MarisUserBundle/login/index.html.twig",[
