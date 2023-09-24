132abba40e8c6c74cf01701ccf94cf4e88c5cf18
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-24 18:27:04 +0300

    52

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,11 +2,13 @@
 
 namespace Maris\Symfony\User\Form;
 
+use Maris\Symfony\User\Entity\User;
 use Misd\PhoneNumberBundle\Form\Type\PhoneNumberType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class RegistrationFormType extends AbstractType
 {
@@ -18,6 +20,13 @@ class RegistrationFormType extends AbstractType
             ->add("password",PasswordType::class)
             ->add("submit",SubmitType::class);
         $builder->get("person")->addViewTransformer( new PersonViewTransformer() );
-        $builder->addViewTransformer( new UserViewTransformer() );
+        //$builder->addViewTransformer( new UserViewTransformer() );
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            "data_class" => User::class
+        ]);
     }
 }
