a457146ddbd24a5474990d15254c5fe9e1429b94
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-27 21:38:07 +0300

    116

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -4,11 +4,8 @@ namespace Maris\Symfony\User\Security;
 
 use Doctrine\ORM\EntityManagerInterface;
 use libphonenumber\PhoneNumber;
-use libphonenumber\PhoneNumberFormat;
 use libphonenumber\PhoneNumberUtil;
-use Maris\Symfony\User\Entity\User;
 use Maris\Symfony\User\Form\LoginFormType;
-use Maris\Symfony\User\Form\UserLoader;
 use Maris\Symfony\User\Repository\UserRepository;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -17,9 +14,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
-use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
-use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -59,6 +54,8 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
         $password = $form->get("password")->getData();
         //$token =  $form->get("_token")->getData();
         $request->getSession()->set(Security::LAST_USERNAME, $login->getNationalNumber() );
+
+        dump( $form->isValid() );
 
         $passport = new Passport(
             new UserBadge($login->getNationalNumber(), $this->userRepository->loadUserByIdentifier(...)),
