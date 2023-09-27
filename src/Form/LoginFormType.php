6be7863a60e072e5bb91b46fe7f578468c7ad95d
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-27 11:03:26 +0300

    97

--- src/Form/LoginFormType.php
+++ src/Form/LoginFormType.php
@@ -33,8 +33,8 @@ class LoginFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            "csrf_token_id" => "authenticate",
-            "csrf_protection" => true
+           // "csrf_token_id" => "authenticate",
+            //"csrf_protection" => true
         ]);
     }
 
