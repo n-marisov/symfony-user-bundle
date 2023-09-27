b98e3541eae53dc0f7fddeeee0b84d8228e715a2
n-marisov <116942294+n-marisov@users.noreply.github.com>
2023-09-27 16:02:38 +0300

    102

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -17,6 +17,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
@@ -39,12 +40,15 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
     private FormFactoryInterface $formFactory;
 
     private UserRepository $userRepository;
-    public function __construct( UrlGeneratorInterface $urlGenerator, UserRepository $userRepository  ,EntityManagerInterface $entityManager, FormFactoryInterface $formFactory )
+
+    private  AuthenticationSuccessHandlerInterface $successHandler;
+    public function __construct( AuthenticationSuccessHandlerInterface $successHandler, UrlGeneratorInterface $urlGenerator, UserRepository $userRepository  ,EntityManagerInterface $entityManager, FormFactoryInterface $formFactory )
     {
         $this->urlGenerator = $urlGenerator;
         $this->entityManager = $entityManager;
         $this->formFactory = $formFactory;
         $this->userRepository = $userRepository;
+        $this->successHandler = $successHandler;
         $this->phoneNumberUtil = PhoneNumberUtil::getInstance();
     }
 
@@ -74,11 +78,12 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
+        return $this->successHandler->onAuthenticationSuccess($request,$token);
         //return null;
-        if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName))
+        /*if ($targetPath = $this->getTargetPath($request->getSession(), $firewallName))
             return new RedirectResponse( $targetPath );
         //return new RedirectResponse( $this->getTargetPath($request->getSession(), $firewallName) );
-        return new RedirectResponse("/");
+        return new RedirectResponse("/");*/
     }
 
     protected function getLoginUrl(Request $request): string
